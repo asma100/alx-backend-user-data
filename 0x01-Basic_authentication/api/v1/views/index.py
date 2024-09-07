6d411e5bed921c883478dd475729aa4cbb5bf1f7
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-09-07 18:08:13 +0000

    task 2 Forbidden => forbidden

--- 0x01-Basic_authentication/api/v1/views/index.py
+++ 0x01-Basic_authentication/api/v1/views/index.py
@@ -26,14 +26,6 @@ def stats() -> str:
     return jsonify(stats)
 
 
-@app_views.route('/unauthorized', methods=['GET'], strict_slashes=False)
-def unauthorized() -> str:
-    """ GET /api/v1/unauthorized
-    """
-    abort(401)
-
-
-
 @app_views.route('/forbidden', methods=['GET'], strict_slashes=False)
 def forbidden() -> str:
     """ GET /api/v1/Forbidden
