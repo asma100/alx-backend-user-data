fc4665818832182419730267e491357b49ef64ee
Asma Elhadi <asmaa.alhai100.gmail.com>
2024-09-11 09:24:22 +0000

    task 6

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -5,22 +5,24 @@ Route module for the API
 from os import getenv
 from api.v1.views import app_views
 from flask import Flask, jsonify, abort, request
-from flask_cors import (CORS, cross_origin)
+from flask_cors import CORS
 import os
 from api.v1.auth.auth import Auth
-from api.v1.auth.basic_auth import BasicAuth
+from api.v1.auth.basic_auth import BasicAuth  # Import BasicAuth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
 
 auth = None
+
+# Determine which authentication type to use based on AUTH_TYPE environment variable
 auth_type = getenv('AUTH_TYPE')
+
 if auth_type == 'basic_auth':
     auth = BasicAuth()
-if auth_type == 'auth':
+elif auth_type == 'auth':
     auth = Auth()
-
 
 @app.before_request
 def before_request():
@@ -30,12 +32,18 @@ def before_request():
     if auth is None:
         return
 
-    excluded_paths = ['/api/v1/status/',
-                      '/api/v1/unauthorized/', '/api/v1/forbidden/']
+    # Paths that do not require authentication
+    excluded_paths = ['/api/v1/status/', '/api/v1/unauthorized/', '/api/v1/forbidden/']
+
+    # Check if the current path requires authentication
     if not auth.require_auth(request.path, excluded_paths):
         return
+
+    # Check if the Authorization header is present
     if auth.authorization_header(request) is None:
         abort(401)
+
+    # Check if the current user is present (authentication)
     if auth.current_user(request) is None:
         abort(403)
 
